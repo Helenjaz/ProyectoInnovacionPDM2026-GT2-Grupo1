02790fb1bb9406a45da93782c539ccc9005abdb1
Helenjaz <al20006@ues.edu.sv>
2026-06-13 12:48:06 -0600

    Proyecto terminado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.proyectoinnovacionpdm2026_gt2_grupo1"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.proyectoinnovacionpdm2026_gt2_grupo1"
-        minSdk = 21
-        targetSdk = 36
+        minSdk = 23
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -29,17 +25,46 @@ android {
             )
         }
     }
+    
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
 dependencies {
+    // AndroidX y Material Design
     implementation(libs.appcompat)
     implementation(libs.material)
-    implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // ML Kit: Reconocimiento de texto y Traducción
+    implementation("com.google.mlkit:text-recognition:16.0.1")
+    implementation("com.google.mlkit:translate:17.0.1")
+
+    // CameraX
+    val camerax_version = "1.2.3"
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation("androidx.camera:camera-view:${camerax_version}")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
+    // Generación de PDF
+    implementation("com.itextpdf:itextpdf:5.5.13.3")
+
+    // Retrofit (Librería 6)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
